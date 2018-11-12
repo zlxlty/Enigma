8497976ee2dd98aed2c932feedb2e0a3376b7b40
SkyLu <zlxlty>
2018-11-12 14:40:59 +0800

    delete extern and strupr

--- src/setting.h
+++ src/setting.h
@@ -2,12 +2,12 @@
 #define _SETTING_H_
 
 // the original fixed setting of an enigma machine
-extern char RotorONE [27] = {"EOVFYPNZGQCMTWHLBXURISKDJA"};
-extern char RotorTWO [27] = {"JCRBIAPSUXHYWOTNZVQDGLFMKE"};
-extern char RotorTHREE [27] = {"OQSUMKAGWIEYNZVXTRPCLJHFDB"};
-extern char RotorFOUR [27] = {"ZYXWVUTSRQPONMLKJIHGFEDCBA"};
-extern char Reflector [27] = {"TAJVWCZFBEIMKOGNXPDLSQHURY"};
-extern char Alpht [27] = {"ZYXWVUTSRQPONMLKJIHGFEDCBA"};
+char RotorONE [27] = {"EOVFYPNZGQCMTWHLBXURISKDJA"};
+char RotorTWO [27] = {"JCRBIAPSUXHYWOTNZVQDGLFMKE"};
+char RotorTHREE [27] = {"OQSUMKAGWIEYNZVXTRPCLJHFDB"};
+char RotorFOUR [27] = {"ZYXWVUTSRQPONMLKJIHGFEDCBA"};
+char Reflector [27] = {"TAJVWCZFBEIMKOGNXPDLSQHURY"};
+char Alpht [27] = {"ZYXWVUTSRQPONMLKJIHGFEDCBA"};
 
 #endif
 
